579f53adbd6c9f50903ba6787d8b0d80001f872c
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-07-10 16:48:33 +0200

    cleanup and correctly set-up locales

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr', 'fa'],
+    locales: ['en', 'fr', 'de', 'es', 'ru', '	pt-T', 'pt-BR', 'zh-Hans'],
     localeConfigs: {
       en: {
         htmlLang: 'en-GB',
