e52eba14b448173eb353db33def8481128c1bbf9
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-07-10 13:29:54 +0200

    try remove broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,15 +80,6 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro"
-              }
-            ]
-          },
-          {
             title: "Community",
             items: [
               {
