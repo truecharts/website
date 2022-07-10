c2d139839a269c8a3b7481983c292ee5931b5579
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-07-10 21:15:16 +0200

    try other syntax for babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  compact: true,
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  presets: [
+    [
+      require.resolve('@docusaurus/core/lib/babel/preset'),
+      {
+        compact: true,
+      }
+    ]
+  ],
 };
