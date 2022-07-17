5a846f8ccc3b728e1cb2596144d51c8ce59b239d
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-07-17 12:23:15 +0200

    try using esbuild loader

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "truecharts", // Usually your GitHub org/user name.
   projectName: "new-pub", // Usually your repo name.
-
+  
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -31,7 +31,16 @@ const config = {
       },
     },
   },
-
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'tsx',
+        format: isServer ? 'cjs' : undefined,
+        target: isServer ? 'node12' : 'es2017',
+      },
+    }),
+  },
   presets: [
     [
       "classic",
