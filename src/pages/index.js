666ab967da09f0c02a3b581d5d22997a92a7f221
Rene Raab <rene.raab@gmx.net>
2022-09-25 13:54:32 +0200

    fix(website): Fixed responsiveness of buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,6 @@ function HomepageHeader() {
             TrueNAS SCALE - 5min ⏱️
           </Link>
           <Link
-            style={{ marginLeft: 2 + "em" }}
             className="button button--secondary button--lg"
             to="/docs/manual/Helm%20Charts/getting-started"
           >
