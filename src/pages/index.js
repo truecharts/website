d160b1d1675c22406b196773ee12094f96050a4b
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2023-01-12 21:45:48 +0100

    Update index.js
    Signed-off-by: Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ function HomepageHeader() {
           Monthly Funding goal:
           <br></br>
         </h3>
-        <ProgressBar bgcolor="#38b856" completed="60" />
+        <ProgressBar bgcolor="#38b856" completed="60" goal="500" />
         <br></br>
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/sponsor">
