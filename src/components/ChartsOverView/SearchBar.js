964afaffafb4163946896c215e7cda272a94011c
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-03-23 07:07:57 -0400

    update img to svg

--- src/components/ChartsOverView/SearchBar.js
+++ src/components/ChartsOverView/SearchBar.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import HelperUtil,{ViewOptions} from './HelperUtil.js';
 import DropDown from './DropDown.js';
-import logo from '@site/static/img/1024.png';
+import logo from '@site/static/svg/favicon.svg';
 
 const SearchBar = ({ placeHolder,searchTerm,handleSearch, setSelectedOption, view}) => {
 
   return (
     <div style={{ display: 'flex', alignItems: 'center', padding: '16px' }}>
-      <img
-        src={logo}
-        alt="TrueCharts logo"
-        width="24"
-        height="24"
-        style={{ marginRight: '0.5em' }}
-      />
+
+    <Svg 
+      src={logo}
+      alt="TrueCharts logo"
+      width="24"
+      height="24"
+      style={{ marginRight: '0.5em' }} />
+      
       <input
         id="search-input"
         type="text"
