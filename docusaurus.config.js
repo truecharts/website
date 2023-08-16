2ef885c9fffce8f0618a32ab7b7eec183b517af9
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2023-08-16 11:23:32 +0200

    move to docusaurus 3.0.0-Alpha.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,27 @@ const config = {
       },
     },
   },
+  markdown: {
+    // By default we compile all files as MDX (allow JSX), but you can opt-in for CommonMark if you want
+    // See https://github.com/facebook/docusaurus/pull/9097
+	// TODO: uncomment on next release
+    // format: 'detect', 
 
+    mdx1Compat: {
+      // Allow html comments in MDX files like blog `<!-- truncate -->` markers
+      comments: true,
+      // Allow former admonition title syntax :::note Title instead of new syntax :::note[Title]
+      admonitions: true,
+      // Allow usage of unescaped {#headingId} syntax instead of \{#headingId}
+      headingIds: true
+    },
+
+    // Gives you opportunity to apply your own string processing before the MDX compilation
+    // Usage is NOT recommended, use this as a last resort escape hatch or temporarily)
+    // preprocessor: ({ filePath, fileContent }) => {
+    //   return fileContent;
+    // }
+  },
   presets: [
     [
       "classic",
@@ -251,6 +271,7 @@ const config = {
       },
     }),
   },
+  
 };
 
 module.exports = config;
