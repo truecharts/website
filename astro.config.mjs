68cd5afc4618d4224c9dcceaefd5d798d11f6783
Kjeld Schouten <info@kjeldschouten.nl>
2024-04-10 12:02:48 +0200

    Apply suggestions from code review

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://charts.truecharts.org",
+  site: "https://test.truecharts.org",
   base: "/",
   output: "static",
   cacheDir: ".astro/cache",
