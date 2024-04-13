cd023ea29e033b6b00a62127aced39a8d6c47afd
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-04-13 13:18:00 +0300

    10 recent posts + fmt (#20)
    **Description**
    <!--
    Please include a summary of the change and which issue is fixed. Please
    also include relevant motivation and context. List any dependencies that
    are required for this change.
    -->
    ⚒️ Fixes  # <!--(issue)-->
    
    **⚙️ Type of change**
    
    - [ ] ⚙️ Feature/App addition
    - [ ] 🪛 Bugfix
    - [ ] ⚠️ Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] 🔃 Refactor of current code
    
    **🧪 How Has This Been Tested?**
    <!--
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    -->
    
    **📃 Notes:**
    <!-- Please enter any other relevant information here -->
    
    **✔️ Checklist:**
    
    - [ ] ⚖️ My code follows the style guidelines of this project
    - [ ] 👀 I have performed a self-review of my own code
    - [ ] #️⃣ I have commented my code, particularly in hard-to-understand
    areas
    - [ ] 📄 I have made corresponding changes to the documentation
    - [ ] ⚠️ My changes generate no new warnings
    - [ ] 🧪 I have added tests to this description that prove my fix is
    effective or that my feature works
    - [ ] ⬆️ I increased versions for any altered app according to semantic
    versioning
    - [ ] I made sure the title starts with `feat(chart-name):`,
    `fix(chart-name):` or `chore(chart-name):`
    
    **➕ App addition**
    
    If this PR is an app addition please make sure you have done the
    following.
    
    - [ ] 🖼️ I have added an icon in the Chart's root directory called
    `icon.png`
    
    ---
    
    _Please don't blindly check all the boxes. Read them and only check
    those that apply.
    Those checkboxes are there for the reviewer to see what is this all
    about and
    the status of this PR with a quick glance._

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    // add color variable
+    extend: {
+      colors: {
+        "tc-primary": "#316ce6",
+      },
+    },
   },
   plugins: [],
 } as Config;
