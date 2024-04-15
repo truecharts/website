5b81982382320279c47119052fefd5b27032b21c
Stavros kois <s.kois@outlook.com>
2024-04-15 10:34:51 +0300

    add chart-list

---
+++ src/components/chart-list/utils.ts
@@ -0,0 +1,25 @@
+import data from "../../assets/charts.json";
+export const normalizeTrainName = (train: string) => {
+  return train.toLowerCase();
+};
+
+export const titleCase = (str: string) => {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+export const getTrains = () => {
+  return data.trains.map((t) => t.name);
+};
+
+export const getChartsFromTrain = (train: string) => {
+  return data.trains.filter((t) => t.name === train)[0].charts;
+};
+
+export const getChartDescription = (chart: string) => {
+  return data.trains
+    .map((t) => t.charts.filter((c) => c.name === chart)[0])
+    .map((c) => c.description)[0];
+};
