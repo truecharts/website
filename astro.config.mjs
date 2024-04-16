5cced0795c4e99a3b8555eb216af81df8ea71c2c
Kjeld Schouten <kjeld@schouten-lebbing.nl>
2024-04-16 12:15:46 +0200

    oops

--- astro.config.mjs
+++ astro.config.mjs
@@ -129,7 +129,7 @@ export default defineConfig({
     }]
   }),
     betterImageService(),
-    astroCompress.default({
+    (await import("@playform/compress")).default({
       HTML: false,
       CSS: false,
       JavaScript: false,
